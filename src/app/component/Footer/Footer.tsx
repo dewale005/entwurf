4887e7d68025f523d2e2098c1db3422245e8e054
Olagunju Adewale <olagunjuadewale5@gmail.com>
2023-03-09 15:13:51 +0000

    fixed footer design

--- src/app/component/Footer/Footer.tsx
+++ src/app/component/Footer/Footer.tsx
@@ -1,14 +1,17 @@
-function Footer () {
-    return (
-        <footer className="bottom-0 bg-red-400 left-0 z-20 w-full p-4 bg-transparent">
-            <div className="container flex flex-wrap items-center justify-between mx-auto">
-    <span className="text-sm text-white sm:text-left font-sansLight">© Sketch Music 2023 All rights reserved. <br /> Design Entwurf
-    </span>
-    <span className="text-sm text-white sm:text-right font-sansLight">Terms & condition<br /> Privacy Policy
-    </span>
-            </div>
-</footer>
-    )
+function Footer() {
+  return (
+    <footer className="bottom-0 bg-red-400 left-0 z-20 w-full p-4">
+      <div className="container flex flex-wrap items-center justify-between mx-auto">
+        <span className="text-sm text-white sm:text-left font-sansLight">
+          © Sketch Music 2023 All rights reserved. <br /> Design Entwurf
+        </span>
+        <span className="text-sm text-white sm:text-right font-sansLight">
+          Terms & condition
+          <br /> Privacy Policy
+        </span>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer 
+export default Footer;
